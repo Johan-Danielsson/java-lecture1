cd18658a8c672d211f326bc731275abe0842e83d
Johan-Danielsson <johan.dunhill@gmail.com>
2024-06-03 14:21:51 +0200

    First push of initial files

---
+++ src/main/java/se/johan/HelloWorld.java
@@ -0,0 +1,21 @@
+package se.johan;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld {
+
+    //This is a main method, used to run java codes. (single line comment)
+    /*
+    this is a multi line comment
+    it can span over multiple lines
+     */
+    public static void main( String[] args ) {
+        // java statement, println=print text in console, make new line
+        System.out.println( "Hello World!" );
+        // print = will write text without making a new line.
+        System.out.print("Hello Java!");
+        System.out.print("Hello Java!");
+    }
+}
